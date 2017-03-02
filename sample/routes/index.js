07fb19b1d43dba41468ca8c74a301449b6e50935
IzumiKyoya <ri5856@yahoo.co.jp>
2017-03-02 11:55:10 +0900

    mofifi to file

--- sample/routes/index.js
+++ sample/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'おなかすいた' ,foods:'そんなことよりおうどん食べたい'});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
